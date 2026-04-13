0d6802c72010a23fc98a1a21a4e41d43ad2fde64
MUKUNDABANTU Mohammed <mkbantu56@gmail.com>
2026-04-13 10:15:26 +0200

    Update my name.c
    editing

--- displaying only my name/my name.c
+++ displaying only my name/my name.c
@@ -2,10 +2,8 @@
 
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main() {
+ main() {
 	printf("Myname is Mukundabantu Mohammed \n");
 	 printf("press enter to exist...");
 	 getchar();
-	return 0;
 }
