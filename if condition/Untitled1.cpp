d67b28dd102a031faec793a10b62f53be3ef9619
mkbantu <mkbantu56@example.com>
2026-04-17 11:42:16 +0200

    using nested if

--- if condition/Untitled1.cpp
+++ if condition/Untitled1.cpp
@@ -1,15 +1,40 @@
-#include<stdio.h>
-int main(){
-  int num;
-  int div;
-  printf("Enter a Number and divisor:\n");
-  scanf("%d %d",&num,&div);
-  if(div!=0){
-	printf("the quotient is:%d",num/div);
+#include <stdio.h>
+#include <string.h>
 
-  }
-  
+int main() {
+    int age, marks;
+    char sex;
+    char nationality[50];
 
-   return 0;	
+    printf("\nPlease enter your age: ");
+    scanf("%d", &age);
+
+    printf("\nPlease enter your marks: ");
+    scanf("%d", &marks);
+
+    printf("\nPlease enter your sex (F or M): ");
+    scanf(" %c", &sex);   // space before %c is important
+
+    printf("\nPlease enter your nationality: ");
+    scanf("%s", nationality);
+
+    if (age >= 30) {
+        if (marks >= 50) {
+            if (sex == 'F' || sex == 'f') {
+                if (strcmp(nationality, "european") == 0) {
+                    printf("recruited");
+                } else {
+                    printf("you are not recruited");
+                }
+            } else {
+                printf("you are not recruited");
+            }
+        } else {
+            printf("you are not recruited");
+        }
+    } else {
+        printf("you are not recruited");
+    }
+
+    return 0;
 }
-
