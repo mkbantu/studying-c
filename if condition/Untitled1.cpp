9dba5973207fe04ad5393505d1d50747accd45bb
mkbantu <mkbantu56@example.com>
2026-04-17 10:34:55 +0200

    using  if else

---
+++ if condition/Untitled1.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+   int age;
+   printf("enter your age:\n");
+   scanf("%d",&age);
+		if(age>=18)
+			{
+			printf("you are allowed to vote\n");
+			}
+		else
+			{
+			printf("you are not allowed\n");
+			
+			}
+   return 0;	
+}
