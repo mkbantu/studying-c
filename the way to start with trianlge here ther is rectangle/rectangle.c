a5b04a6c02dd1be2c3af4f260bcc914e8c9566b1
mkbantu <mkbantu56@example.com>
2026-04-24 11:43:04 +0200

    lower corner triangle

--- the way to start with trianlge here ther is rectangle/rectangle.c
+++ the way to start with trianlge here ther is rectangle/rectangle.c
@@ -4,8 +4,9 @@ void main(){
     int j;
     int i;
     for(i=0;i<=10;i++){
-        for(j=0;j<=i;j++){
+        for(j=0;j<=(i-1);j++){
             printf("  %d",i);
+            
         }
     printf("\n");
 
