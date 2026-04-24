153441d7b27d152d883b6ef52c1bb33cca33e16d
mkbantu <mkbantu56@example.com>
2026-04-24 10:25:15 +0200

    finishing

---
+++ displaying number out of 40-50 range in 100/range.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+        int i=0;
+        for(i<=100,i++){
+            if(i<=50 && i>=40){
+                continue;
+            }
+        printf("%d\n",i)
+        }
+    return 0;
+}
