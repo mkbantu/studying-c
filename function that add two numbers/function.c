fcfc507ff8fe9c6dafed3d8b9b9b1ad2eca720ae
mkbantu <mkbantu56@example.com>
2026-05-08 11:45:29 +0200

    function that perfom all opeartion

--- function that add two numbers/function.c
+++ function that add two numbers/function.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int add(int a, int b) {
-    return a + b;
-}
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
 
 int main() {
     int x, y;
@@ -10,6 +11,31 @@ int main() {
     scanf("%d", &x);
     printf("Enter second number: ");
     scanf("%d", &y);
+
     printf("%d + %d = %d\n", x, y, add(x, y));
+    printf("%d - %d = %d\n", x, y, subtract(x, y));
+    printf("%d * %d = %d\n", x, y, multiply(x, y));
+
+    if (y != 0)
+        printf("%d / %d = %.2f\n", x, y, divide(x, y));
+    else
+        printf("Division by zero is not allowed.\n");
+
     return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+float divide(int a, int b) {
+    return (float)a / b;
 }
