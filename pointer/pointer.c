078b8ac0a31fb36ea511b5f62184360bef6e2343
mkbantu <mkbantu56@gmail.com>
2026-06-05 11:34:09 +0200

    file

--- pointer/pointer.c
+++ pointer/pointer.c
@@ -1,4 +1,11 @@
 #include<stdio.h>
 int main(){
-    
+    FILE *fn;
+    int x;
+    fn=fopen("mukundabntu","w");
+    for(x=9;x>=0;x--){
+        fprintf(fn,"%d\n",x);
+    }
+    close(fn);
+    return 0;
 }
